209ce6a49d696583b60436b56faa332d40e245fc
Lingbin Sun <31414004+akirayukimoto@users.noreply.github.com>
2018-11-28 18:39:08 -0500

    Unused script update

---
+++ New Unity Project (2)/Assets/sticky.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sticky : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.transform.parent = gameObject.transform;
+    }
+}
