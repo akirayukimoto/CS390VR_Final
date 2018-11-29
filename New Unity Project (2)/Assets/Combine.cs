e533be297d683aa132ab5a4d9794957e84288271
Lingbin Sun <31414004+akirayukimoto@users.noreply.github.com>
2018-11-29 14:30:14 -0500

    Edit "Combine"

--- New Unity Project (2)/Assets/Combine.cs
+++ New Unity Project (2)/Assets/Combine.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Combine : MonoBehaviour
 {
     public int stuff_count;
+    public int num_fruits;
+    public int num_foods;
+    public int num_drinks;
+    public int total_price;
     // Use this for initialization
     void Start()
     {
         stuff_count = 0;
+        num_fruits = 0;
+        num_foods = 0;
+        num_drinks = 0;
+        total_price = 0;
     }
-
-    // Update is called once per frame
-    //void Update () {
-
-    //}
 
     void OnCollisionEnter(Collision col)
     {
@@ -24,6 +27,20 @@ public class Combine : MonoBehaviour
         {
             col.gameObject.transform.parent = this.transform;
             stuff_count++;
+
+            if (col.gameObject.tag.Equals("Foods"))
+            {
+                num_foods++;
+            }
+            else if (col.gameObject.tag.Equals("Fruits"))
+            {
+                num_fruits++;
+            }
+            else if (col.gameObject.tag.Equals("drinks"))
+            {
+                num_drinks++;
+            }
+            total_price = num_fruits * 3 + num_foods * 5 + num_drinks * 3;
         }
         if (stuff_count == 2)
         {
