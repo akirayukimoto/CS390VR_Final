b621c763d41e34242b5a3926aa531cfe29ae3c98
Lingbin Sun <31414004+akirayukimoto@users.noreply.github.com>
2018-12-06 13:12:18 -0500

    Update AddSomething.cs

--- New Unity Project (2)/Assets/AddSomething.cs
+++ New Unity Project (2)/Assets/AddSomething.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AddSomething : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void OnTriggerEnter(Collider col)
+public class AddSomething : MonoBehaviour
+{
+    public Text text;
+    // Use this for initialization
+    void Start()
     {
-        //Debug.Log(col.gameObject.tag);
-        col.gameObject.transform.parent = this.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int total = 0;
+        foreach (Transform child in this.gameObject.transform)
+        {
+            if (child.name.Equals("ShoppingCart"))
+            {
+            }
+            else
+            {
+                total++;
+            }
+
+        }
+        text.text = "Curr:" + total.ToString();
+
     }
 }
